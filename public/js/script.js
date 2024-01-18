915d3e98e2f8077281889cc7cc139520de841c48
Vanessa Fayard <vanessa.fayard@gmail.com>
2024-01-18 13:24:10 +0100

    Init webdesign css + webfont project

---
+++ public/js/script.js
@@ -0,0 +1,13 @@
+// const toggleButton = document.getElementById("toggleColorMode");
+
+// toggleButton.addEventListener('click', function() {
+//     document.body.classList.toggle('dark-mode');
+//     const currentMode = document.body.classList.contains('dark-mode') ? 'dark' : 'light';
+//     localStorage.setItem('preferred-mode', currentMode); 
+// });
+
+
+// const preferredMode = localStorage.getItem('preferred-mode');
+// if (preferredMode === 'dark') {
+//     document.body.classList.add('dark-mode');
+// }
