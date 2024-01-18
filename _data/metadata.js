cb2e6bf0266691a3ba4333f5e619e69f99565f58
Vanessa Fayard <vanessa.fayard@gmail.com>
2024-01-18 13:20:17 +0100

    Init eleventy blog project

---
+++ _data/metadata.js
@@ -0,0 +1,11 @@
+module.exports = {
+	title: "Blog Vanessa Fayard",
+	url: "https://example.com/",
+	language: "fr",
+	description: "Blog où je dépose des billets de partage, retour d'expérience, perso, autour de mon parcours professionnel et du développement web",
+	author: {
+		name: "Vanessa Fayard",
+		email: "vanessa.fayard@gmail.com",
+		url: "https://example.com/about-me/"
+	}
+}
