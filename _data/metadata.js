fb85bc4f27103f23a3c2636c9486fbd540b28563
Vanessa Fayard <vanessa.fayard@gmail.com>
2024-02-09 11:20:24 +0100

    Fix pages, css, and configuration after deploy

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 module.exports = {
 	title: "Vanessa Fayard",
-	url: "https://example.com/",
+	url: "https://vanessafayard.github.io/",
 	language: "fr",
 	description: "Blog où je dépose des billets de partage, retour d'expérience, perso, autour de mon parcours professionnel et du développement web",
 	author: {
 		name: "Vanessa Fayard",
 		email: "vanessa.fayard@gmail.com",
-		url: "https://example.com/about-me/"
+		url: "https://vanessafayard.github.io/about/"
 	}
 }
